98720a7451a6ef7345d6a127eaf5e306d70aa2ef
kylebusey <kyledbuseybusiness@gmail.com>
2024-08-21 13:42:47 -0400

    Initial commmit

--- src/test/java/com/pvptracker/PVPTrackerPluginTest.java
+++ src/test/java/com/pvptracker/PVPTrackerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.pvptracker;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PVPTrackerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PVPTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
